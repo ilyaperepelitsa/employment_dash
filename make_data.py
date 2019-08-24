20dd5f7487c207b48815c05c072b543aba85b55d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.local>
2019-08-24 11:45:20 +0300

    init

---
+++ make_data.py
@@ -0,0 +1,22 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+import re
+import selenium
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+
+
+def init_driver():
+    path_to_chromedriver = "chromedriver"
+    driver = webdriver.Chrome(executable_path = path_to_chromedriver)
+    driver.wait = WebDriverWait(driver, 5)
+    return driver
